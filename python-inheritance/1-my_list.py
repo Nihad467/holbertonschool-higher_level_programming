928f7fbb92f2c418c1f1751c76d366a9bfccaa06
Nihad467 <nihadseferzade01@gmail.com>
2026-05-09 16:51:02 +0400

    Add MyList class and tests

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,10 @@
-﻿#!/usr/bin/python3
-"""Module that defines MyList class."""
+#!/usr/bin/python3
+"""Module that defines a list subclass."""
 
 
 class MyList(list):
-    """A class that inherits from list."""
+    """A custom list class that inherits from list."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """Print the list in ascending sorted order."""
         print(sorted(self))
-
