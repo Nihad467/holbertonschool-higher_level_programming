c845eb4b515c23565a5a49063b422f5833bf09be
Nihad467 <nihadseferzade01@gmail.com>
2026-05-09 19:56:30 +0400

    Add Student JSON filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation of a Student instance."""
+        if type(attrs) is list:
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs and type(key) is str
+            }
+        return self.__dict__
