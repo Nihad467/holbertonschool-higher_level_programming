a9492bee10ae89fc0151a7ff386b5d49e2531877
Nihad467 <nihadseferzade01@gmail.com>
2026-05-09 19:44:17 +0400

    Fix append write PEP8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,9 @@
 
 
 def append_write(filename="", text=""):
-    """Append text to a UTF-8 file and return the number of characters added."""
+    """Append text to a UTF-8 file.
+
+    Return the number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
