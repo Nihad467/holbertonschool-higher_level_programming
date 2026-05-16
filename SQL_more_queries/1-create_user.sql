ee1fcfd84d4ee8b7091cb00dd2424817e0f14ad4
Nihad467 <nihadseferzade01@gmail.com>
2026-05-16 19:33:42 +0400

    Add SQL script to create user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
-﻿-- Creates MySQL user user_0d_1 with all privileges
+-- Create user_0d_1 with all privileges if the user does not exist.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
